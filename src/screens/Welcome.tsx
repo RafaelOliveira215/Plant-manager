66eb2287528ea03c6ed6f76ceaf235039ba46400
RafaelOliveira215 <rafaelllmoliveira@gmail.com>
2021-04-21 15:37:07 -0300

    finalização das telas iniciais e adicionado react navigator

--- src/screens/Welcome.tsx
+++ src/screens/Welcome.tsx
@@ -1,47 +1,62 @@
 import React, {useState} from "react"
-import {Text, SafeAreaView, StyleSheet, Image, TouchableOpacity} from "react-native"
+import {Text, SafeAreaView, StyleSheet, Image, TouchableOpacity,Dimensions, View} from "react-native"
 
 import watetingImg from "../assets/watering.png"
 import colors from "../styles/colors"
-import {Button} from "../components/Button"
+import {Feather} from '@expo/vector-icons'
+import fonts from "../styles/fonts"
+import { useNavigation } from "@react-navigation/core"
 
 export function Welcome(){
-   const [visible,setVisible] = useState(false)
-
-   const handleVisibility = ()=>{
-       setVisible(true)
-   }
+    const Navigation = useNavigation()
+    
+    function handleStart(){
+        Navigation.navigate('UserIdentification')
+    }
 
   return(
     <SafeAreaView style={styles.container}>
+        <View style={styles.wrapper}>
       <Text style={styles.title}>Gerencie {`\n`}
-       suas plantas {`\n`}
-       de forma fácil</Text>
-   { visible && <Image style={styles.image} source={watetingImg}/>}
+       suas plantas de {`\n`}
+     forma fácil</Text>
+   <Image resizeMode="contain" style={styles.image} source={watetingImg}/>
     <Text style={styles.subtitle}>Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você sempre que precisar. </Text>
-    <Button onPress={handleVisibility} title=">"/>
+    <TouchableOpacity onPress={handleStart} activeOpacity={0.7} style={styles.button}>
+        <Text >
+            <Feather style={styles.buttonIcon} name="chevron-right"/>
+        </Text>
+        </TouchableOpacity>
+        </View>
     </SafeAreaView> 
   )
 }
 
 const styles = StyleSheet.create({
     container:{
+      flex:1
+    },
+    wrapper:{
         flex:1,
         alignItems:`center`,
-        justifyContent:`space-between`
+        justifyContent:`space-around`,
+        paddingHorizontal:20
     },
     title:{
-        fontSize:32,
+        fontSize:28,
         fontWeight: `bold`,
         textAlign:`center`,
         color:colors.heading,
-        marginTop:38
+        marginTop:38,
+        fontFamily:fonts.heading,
+        lineHeight:34
     },
     subtitle:{
         textAlign:`center`,
         fontSize:18,
         paddingHorizontal:20,
-        color:colors.heading
+        color:colors.heading,
+        fontFamily:fonts.text
     },
     button:{
         backgroundColor:colors.green,
@@ -50,13 +65,12 @@ const styles = StyleSheet.create({
         borderRadius:16,
         marginBottom:10,
         height:56,
-        paddingHorizontal:10
+        width:56
     },
     image:{
-        width:292,
-        height:284
+        height: Dimensions.get(`window`).width * 0.7
     },
-    buttonText:{
+    buttonIcon:{
         color:colors.white,
         fontSize:24
     }
